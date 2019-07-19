22618e6362e1f850eeb0bb091cce8c7cbddd6840
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-07-19 10:01:39 +0530

    init

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,26 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Carbon\Carbon;
-use App\Category;
 
 class TestController extends Controller
 {
     public function test(Request $request)
     {
-        // $categories = collect(config('category'))
-        //     ->map(function ($category) {
-        //         return [
-        //             'parent_id' => 4,
-        //             'name' => $category,
-        //             'created_at' => Carbon::now(),
-        //             'updated_at' => Carbon::now(),
-        //         ];
-        //     })
-        //     ->toArray();
-
-        // $insert = Category::insert($categories);
-
-        // return response(['status' => $insert], 200);
+        return 'done';
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div style="border: 1px solid #ddd; padding: 10px">
-            <a href="/questions">Questions</a>
-            <span>|</span>
-            <a href="/exams">Exams</a>
-        </div>
-
-        @if(auth()->user())
-            <div style="margin-top: 20px">
-                Welcome, {{ ucwords(auth()->user()->name) }}
-            </div>
-        @else
-            <div style="margin-top: 20px">Welcome, Guest</div>
-        @endif
+<div class="container">
+    <div style="border: 1px solid #ddd; padding: 10px">
+        <a href="/questions">Questions</a>
     </div>
+
+    @if(auth()->user())
+    <div style="margin-top: 20px">
+        Welcome, {{ ucwords(auth()->user()->name) }}
+    </div>
+    @else
+    <div style="margin-top: 20px">Welcome, Guest</div>
+    @endif
+</div>
 @endsection
